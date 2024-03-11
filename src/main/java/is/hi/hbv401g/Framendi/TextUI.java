0b75f90261f5498054d0cd03c9f03a846252ac48
Maria Ros <mrk9@hi.is>
2024-03-11 14:11:08 +0000

    TextUI

--- src/main/java/is/hi/hbv401g/Framendi/TextUI.java
+++ src/main/java/is/hi/hbv401g/Framendi/TextUI.java
@@ -1,4 +1,21 @@
 package is.hi.hbv401g.Framendi;
+import is.hi.hbv401g.Bakendi.*;
 
 public class TextUI {
+    public static void main(String[] args) {
+        TextUI ui = new TextUI();
+        ui.run();
+    }
+
+    private void run() {
+        System.out.println("Book a flight: ");
+        System.out.println("Available departure cities: ");
+        //Flight flight = new Flight(getUserInput());
+
+
+    }
+
+    private String getUserInput() {
+        return null;
+    }
 }
