4b8d979eb1ca65a300cfe7e6fb9fdd9e93122337
manifreyr <manifreyrr@gmail.com>
2024-03-11 15:52:31 +0000

    Nokkrar aðferðir í BookingController

--- src/main/java/is/hi/hbv401g/Bakendi/BookingController.java
+++ src/main/java/is/hi/hbv401g/Bakendi/BookingController.java
@@ -1,9 +1,19 @@
 package is.hi.hbv401g.Bakendi;
 
 public class BookingController {
+    private BookingRepository bookingRepository;
 
     public void createBooking(User user, Flight[] flights,String bookingID,Seat[] seats) {
         Booking booking = new Booking(user, flights, bookingID, seats);
+        //bookingRepository.createBooking(booking);
+    }
+    public void updateBooking(User user, Flight[] flights,String bookingID,Seat[] seats) {
+        Booking booking = new Booking(user, flights, bookingID, seats);
+        //bookingRepository.updateBooking(booking);
+    }
+    public void deleteBooking(User user, Flight[] flights,String bookingID,Seat[] seats) {
+        Booking booking = new Booking(user, flights, bookingID, seats);
+        //bookingRepository.deleteBooking(booking);
     }
 
 
