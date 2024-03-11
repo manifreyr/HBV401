bbf72a37409c786ec83f732e6c821252425b59ff
Maria Ros <mrk9@hi.is>
2024-03-11 20:53:47 +0000

    Blabla

--- src/main/java/is/hi/hbv401g/Bakendi/FlightRepository.java
+++ src/main/java/is/hi/hbv401g/Bakendi/FlightRepository.java
@@ -1,9 +1,15 @@
 package is.hi.hbv401g.Bakendi;
 
 import java.sql.Connection;
+import java.util.List;
 
 public class FlightRepository {
     Connection connection = DatabaseConnector.connect();
 
+    public List<Flight> getAllFlights() {
+        String sql = "SELECT * FROM Flight";
+
+
+    }
 
 }
