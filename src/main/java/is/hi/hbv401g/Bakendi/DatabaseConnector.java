2992b4f284f5c1fb167775aa764c1cc374f93090
Maria Ros <mrk9@hi.is>
2024-03-11 13:27:35 +0000

    DatabaseConnector

---
+++ src/main/java/is/hi/hbv401g/Bakendi/DatabaseConnector.java
@@ -0,0 +1,11 @@
+package is.hi.hbv401g.Bakendi;
+
+import java.sql.Connection;
+
+public class DatabaseConnector {
+
+    String url = "jdbc:sqlite:src/main/resources/flightBooking.db";
+    public static Connection connect(){
+        return null;
+    }
+}
