f4cba02d3210a1995445e3b52652c4696e230286
Maria Ros <mrk9@hi.is>
2024-03-11 13:39:49 +0000

    DatabaseConnector basic

--- src/main/java/is/hi/hbv401g/Bakendi/DatabaseConnector.java
+++ src/main/java/is/hi/hbv401g/Bakendi/DatabaseConnector.java
@@ -1,11 +1,22 @@
 package is.hi.hbv401g.Bakendi;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class DatabaseConnector {
-
-    String url = "jdbc:sqlite:src/main/resources/flightBooking.db";
     public static Connection connect(){
-        return null;
+        String url = "jdbc:sqlite:src/main/resources/flightBooking.db";
+        Connection connection = null;
+        try {
+            connection = java.sql.DriverManager.getConnection(url);
+            System.out.println("Connection to SQLite has been established."+connection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
+    public static void main(String[] args){
+        connect();
     }
 }
