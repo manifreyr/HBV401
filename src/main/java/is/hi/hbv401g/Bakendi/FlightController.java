6dda1b2d1a570047c48ca9331b469896773bea71
Maria Ros <mrk9@hi.is>
2024-04-09 16:38:51 +0000

    Return available flights

--- src/main/java/is/hi/hbv401g/Bakendi/FlightController.java
+++ src/main/java/is/hi/hbv401g/Bakendi/FlightController.java
@@ -8,92 +8,117 @@ import java.util.List;
 
 public class FlightController {
     private final FlightRepository flightRepository = new FlightRepository();
+
     public void createFlight(Flight flight) throws SQLException {
         flightRepository.addFlight(flight);
     }
-    public void updateFlightDate(Flight flight, LocalDate date){
+
+    public void updateFlightDate(Flight flight, LocalDate date) {
         flight.setDay(date);
     }
-    public void updateFlightPrice(Flight flight, int price){
+
+    public void updateFlightPrice(Flight flight, int price) {
         flight.setPrice(price);
     }
+
     public Flight getFlightByNumber(String flightNumber) throws SQLException {
-        for(Flight flight: flightRepository.getAllFlights()){
-            if(flight.getFlightNumber().equals(flightNumber)){
+        for (Flight flight : flightRepository.getAllFlights()) {
+            if (flight.getFlightNumber().equals(flightNumber)) {
                 return flight;
             }
         }
         return null;
     }
+
     public List<Flight> getFlightsByDate(LocalDate date) throws SQLException {
         List<Flight> flights = new ArrayList<>();
-        for(Flight flight: flightRepository.getAllFlights()){
-            if(flight.getDay().equals(date)){
+        for (Flight flight : flightRepository.getAllFlights()) {
+            if (flight.getDay().equals(date)) {
                 flights.add(flight);
             }
         }
         return flights;
     }
+
     public List<Flight> getAllFlights() throws SQLException {
         return flightRepository.getAllFlights();
     }
+
     public List<Flight> getFlightsByDepartureCity(String departureCity) throws SQLException {
         List<Flight> flights = new ArrayList<>();
-        for(Flight flight: flightRepository.getAllFlights()){
-            if(flight.getDepartureCity().equals(departureCity)){
+        for (Flight flight : flightRepository.getAllFlights()) {
+            if (flight.getDepartureCity().equals(departureCity)) {
                 flights.add(flight);
             }
         }
         return flights;
     }
+
     public List<Flight> getFlightsByArrivalCity(String arrivalCity) throws SQLException {
         List<Flight> flights = new ArrayList<>();
-        for(Flight flight: flightRepository.getAllFlights()){
-            if(flight.getArrivalCity().equals(arrivalCity)){
+        for (Flight flight : flightRepository.getAllFlights()) {
+            if (flight.getArrivalCity().equals(arrivalCity)) {
                 flights.add(flight);
             }
         }
         return flights;
     }
+
     public boolean flightExists(Flight flight) throws SQLException {
-        for(Flight f: flightRepository.getAllFlights()){
-            if(f.getFlightNumber().equals(flight.getFlightNumber())){
+        for (Flight f : flightRepository.getAllFlights()) {
+            if (f.getFlightNumber().equals(flight.getFlightNumber())) {
                 return true;
             }
         }
         return false;
     }
-    public void sortFlightsByPrice(List<Flight> flights){
+
+    public void sortFlightsByPrice(List<Flight> flights) {
         flights.sort(Comparator.comparingInt(Flight::getPrice));
     }
-    public void sortFlightsByDate(List<Flight> flights){
+
+    public void sortFlightsByDate(List<Flight> flights) {
         flights.sort(Comparator.comparing(Flight::getDay));
     }
-    public void sortFlightsByDepartureCity(List<Flight> flights){
+
+    public void sortFlightsByDepartureCity(List<Flight> flights) {
         flights.sort(Comparator.comparing(Flight::getDepartureCity));
     }
-    public void sortFlightsByArrivalCity(List<Flight> flights){
+
+    public void sortFlightsByArrivalCity(List<Flight> flights) {
         flights.sort(Comparator.comparing(Flight::getArrivalCity));
     }
-    public void deleteFlight(Flight flight){
+
+    public void deleteFlight(Flight flight) {
         flightRepository.deleteFlight(flight);
     }
+
     public List<String> getAllDepartures() throws SQLException {
         List<String> departures = new ArrayList<>();
-        for(Flight flight: flightRepository.getAllFlights()){
-            if(!departures.contains(flight.getDepartureCity())){
+        for (Flight flight : flightRepository.getAllFlights()) {
+            if (!departures.contains(flight.getDepartureCity())) {
                 departures.add(flight.getDepartureCity());
             }
         }
         return departures;
     }
+
     public List<String> getAllArrivals() throws SQLException {
         List<String> arrivals = new ArrayList<>();
-        for(Flight flight: flightRepository.getAllFlights()){
-            if(!arrivals.contains(flight.getArrivalCity())){
+        for (Flight flight : flightRepository.getAllFlights()) {
+            if (!arrivals.contains(flight.getArrivalCity())) {
                 arrivals.add(flight.getArrivalCity());
             }
         }
         return arrivals;
     }
+    public List<Flight> returnAvailableFlights(String departureCity, String arrivalCity, LocalDate date) throws SQLException {
+        List<Flight> availableFlights = new ArrayList<>();
+        for (Flight flight : flightRepository.getAllFlights()) {
+            if (flight.getDepartureCity().equals(departureCity) && flight.getArrivalCity().equals(arrivalCity) && flight.getDay().equals(date)) {
+                availableFlights.add(flight);
+            }
+        }
+        return availableFlights;
     }
+}
