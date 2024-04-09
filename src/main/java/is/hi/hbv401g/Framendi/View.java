68cee702d7ad666671ed02c0e5a8e66609baa40e
Ari Gunnar Kristjónsson <agk19@hi.is>
2024-04-09 16:41:16 +0000

    final of flight

--- src/main/java/is/hi/hbv401g/Framendi/View.java
+++ src/main/java/is/hi/hbv401g/Framendi/View.java
@@ -4,7 +4,9 @@ package is.hi.hbv401g.Framendi;
 public enum View {
     OPNA("/is.hi.hbvg401g.Framendi/Forsida.fxml"),
     DETAIL("/is.hi.hbvg401g.Framendi/passenger.fxml"),
-    FLIGHT("/is.hi.hbvg401g.Framendi/Flight.fxml");
+    FLIGHT("/is.hi.hbvg401g.Framendi/Flight.fxml"),
+    FINAL("/is.hi.hbvg401g.Framendi/Final.fxml");
+
 
     private String fileName;
 
