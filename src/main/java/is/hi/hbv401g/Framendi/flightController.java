4ae984d6330a56e6e8baf3978ce018e78ec6d98a
Ari Gunnar Kristjónsson <agk19@hi.is>
2024-04-09 14:59:05 +0000

    annar takki nuna á flight hehe

--- src/main/java/is/hi/hbv401g/Framendi/flightController.java
+++ src/main/java/is/hi/hbv401g/Framendi/flightController.java
@@ -1,10 +1,15 @@
 package is.hi.hbv401g.Framendi;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 public class flightController {
 
-
+    @FXML
+    void pastScene(ActionEvent event) {
+        System.out.print("smellt á takka");
+        ViewSwitcher.switchTo(View.DETAIL);
+    }
 
 }
