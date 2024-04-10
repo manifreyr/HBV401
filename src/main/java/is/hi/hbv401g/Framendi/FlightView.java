40530e9d89e5ec11458546da96d139c633bb7785
Maria Ros <mrk9@hi.is>
2024-04-10 14:03:56 +0000

    FlightView constructor

--- src/main/java/is/hi/hbv401g/Framendi/FlightView.java
+++ src/main/java/is/hi/hbv401g/Framendi/FlightView.java
@@ -1,11 +1,20 @@
 package is.hi.hbv401g.Framendi;
-
-import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.VBox;
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
+
 
 public class FlightView extends VBox {
-    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("FlightView.fxml"));
-    fxmlLoader.setRoot(this);
-    fxmlLoader.setController(this);
-
+    public FlightView() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("FlightView.fxml"));
+            fxmlLoader.setRoot(this);
+            fxmlLoader.setController(this);
+            fxmlLoader.load(); // Don't forget to call load()
+        } catch (IOException e) {
+            // Handle exceptions
+            e.printStackTrace();
+        }
+    }
 }
