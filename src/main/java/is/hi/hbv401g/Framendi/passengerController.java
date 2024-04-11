073b5e4ea6ea33114bf2e5e31b008d3374db21da
Ari Gunnar Kristjónsson <agk19@hi.is>
2024-04-11 17:08:34 +0000

    ógeðsleg passenger controller/fxml ég hata tabpane og alla sem hanga með henni >:(

--- src/main/java/is/hi/hbv401g/Framendi/passengerController.java
+++ src/main/java/is/hi/hbv401g/Framendi/passengerController.java
@@ -4,75 +4,41 @@ import javafx.beans.Observable;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import org.w3c.dom.Text;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class passengerController {
 
     @FXML
-    void pastScene(ActionEvent event) {
-        ViewSwitcher.switchTo(View.OPNA);
-    }
+    private TextField firstName;
+    @FXML
+    private TextField lastName;
+    @FXML
+    private TextField phone;
+    @FXML
+    private TextField ssn;
 
     @FXML
-    void nextScene(ActionEvent event) {
+    void saveUserInfo(ActionEvent event) {
+
+        String first = firstName.getText();
+        String last = lastName.getText();
+        String phoneNumber = phone.getText();
+        String ssnNumber = ssn.getText();
+
+
+        ViewSwitcher.switchTo(View.FINAL);
+    }
+    @FXML
+    void pastScene(ActionEvent event){
         ViewSwitcher.switchTo(View.FLIGHT);
     }
-
-
-
-    @FXML TabPane tabPane;
-    @FXML Tab passenger1;
-    @FXML
-    private Label numPassengers;
-    private int passengerCount = 1;
-
-
-    @FXML
-    public void plusPassenger(ActionEvent event){
-        passengerCount++;
-        updatePassengerCount();
-        cloneGrid();
-        System.out.print("nýr tab");
-    }
-    @FXML
-    void minusPassenger(ActionEvent event) {
-        if (passengerCount > 1) {
-            passengerCount--;
-            updatePassengerCount();
-            ObservableList<Tab> tabs = tabPane.getTabs();
-            Tab lastTab = tabs.get(tabs.size() - 1);
-            tabs.remove(lastTab);
-            System.out.print(" kúkapiss ");
-        }
-
-
-    }
-    private void updatePassengerCount() {
-        numPassengers.setText(String.valueOf(passengerCount));
-    }
-    private void cloneGrid(){
-        Tab newTab = new Tab("Passenger" + passengerCount);
-
-        GridPane clonedGridPane = new GridPane();
-        clonedGridPane.getChildren().addAll(passenger1.getContent());
-
-        newTab.setContent(clonedGridPane);
-        tabPane.getTabs().add(newTab);
-
-        //velur nýjasta tabinn
-        tabPane.getSelectionModel().select(newTab);
-    }
-
-
-
-
-
-
-
-
-
 }
+
