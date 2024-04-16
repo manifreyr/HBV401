5a251974fc70a722a7d53697bd299bf55616ed10
manifreyr <manifreyrr@gmail.com>
2024-04-16 20:22:25 +0000

    Styles

--- src/main/java/is/hi/hbv401g/Framendi/FinalReturnController.java
+++ src/main/java/is/hi/hbv401g/Framendi/FinalReturnController.java
@@ -3,6 +3,7 @@ package is.hi.hbv401g.Framendi;
 import is.hi.hbv401g.Bakendi.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
@@ -48,13 +49,16 @@ public class FinalReturnController {
     private passengerController pController;
     private FlightListController fController;
     private ReturnController rController;
+    private forsidaController forsidaController;
     private UserRepository userRepository = new UserRepository();
     private FlightRepository flightRepository = new FlightRepository();
     private BookingController bookingController = new BookingController();
+    private BookingRepository bookingRepository = new BookingRepository();
 
     private List<User> users;
 
     public void initialize(){
+        forsidaController = (forsidaController) ViewSwitcher.lookup(View.OPNA);
         pController = (passengerController) ViewSwitcher.lookup(View.DETAIL);
         fController = (FlightListController) ViewSwitcher.lookup(View.FLIGHT);
         rController = (ReturnController) ViewSwitcher.lookup(View.RETURN);
@@ -98,8 +102,39 @@ public class FinalReturnController {
             flightRepository.decreaseAvailableSeats(fController.getSelectedOutBoundFlight());
             bookingController.createBooking(fController.getSelectedOutBoundFlight(), user);
         }
+    dialog();
 
-
+    }
+    public void frontPage(){
+        forsidaController.clear();
+        ViewSwitcher.switchTo(View.OPNA);
+    }
+    public void dialog(){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Booking Confirmation");
+        alert.setHeaderText(null);  // No header text
+        alert.setContentText(dialogText());
+        alert.setOnHidden(evt -> frontPage());
+        alert.showAndWait();
+    }
+    public String dialogText() {
+        StringBuilder message = new StringBuilder();
+        if(users.size() == 1){
+            message.append("You have confirmed a booking for:").append("\n");
+        }
+        else{
+            message.append("You have confirmed bookings for:").append("\n");
+        }
+        if (users.size() == 1) {
+            message.append(users.get(0).getUserName()).append("\n").append("Booking ID: ")
+                    .append(bookingRepository.getBookingByFlightNrAndUserSSNo(fController.getSelectedOutBoundFlight().getFlightNumber(),users.get(0).getUserID()).getBookingID());
+        } else {
+            for (User user : users) {
+                message.append(user.getUserName()).append("\n").append("Booking ID: ")
+                        .append(bookingRepository.getBookingByFlightNrAndUserSSNo(fController.getSelectedOutBoundFlight().getFlightNumber(), user.getUserID()).getBookingID()).append("\n").append("\n");
+            }
+        }
+        return message.toString();
     }
     public void changeUser(MenuItem item){
         fxName.setText(users.get(Integer.parseInt(item.getText()) - 1).getFirstName() + " " + users.get(Integer.parseInt(item.getText()) - 1).getLastName());
